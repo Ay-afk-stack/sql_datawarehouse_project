68d7e0b4269b2117a1ffe6c6488e7d69a840c5f2
Ay-afk-stack <pakhrin.ayush404@gmail.com>
2026-01-04 07:57:06 +0000

    bulk inserted into tables

--- scripts/init_tables.session.sql
+++ scripts/init_tables.session.sql
@@ -31,7 +31,7 @@ CREATE TABLE bronze.crm_prd_info (
 
 DROP TABLE IF EXISTS bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details (
-    sls_ord_num INT,
+    sls_ord_num VARCHAR(50),
     sls_prd_key VARCHAR(50),
     sls_cust_id INT,
     sls_order_dt INT,
