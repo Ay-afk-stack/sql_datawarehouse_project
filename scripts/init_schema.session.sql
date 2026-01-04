8254f76c77f67673e52dae995ed75b7f81c8da6b
Ay-afk-stack <pakhrin.ayush404@gmail.com>
2026-01-04 07:23:22 +0000

    validating schema & tables

--- scripts/init_schema.session.sql
+++ scripts/init_schema.session.sql
@@ -6,11 +6,13 @@ Script Purpose:
     This script creates a new schema
 */
 
--- Dropping Schema
 DROP SCHEMA IF EXISTS bronze;
-
 CREATE SCHEMA bronze;
+
+DROP SCHEMA IF EXISTS silver;
 CREATE SCHEMA silver;
+
+DROP SCHEMA IF EXISTS gold;
 CREATE SCHEMA gold;
 
 
--- scripts/init_tables.session.sql
+++ scripts/init_tables.session.sql
@@ -7,6 +7,7 @@ Script Purpose:
     Bronze layer rule i.e. <layer_name>.<sourcesystem>_<entity | table_name>
 */
 
+DROP TABLE IF EXISTS bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info (
     cst_id INT,
     cst_key VARCHAR(50),
@@ -17,6 +18,7 @@ CREATE TABLE bronze.crm_cust_info (
     cst_create_date DATE
 );
 
+DROP TABLE IF EXISTS bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info (
     prd_id INT,
     prd_key VARCHAR(50),
@@ -27,6 +29,7 @@ CREATE TABLE bronze.crm_prd_info (
     prd_end_dt DATE
 );
 
+DROP TABLE IF EXISTS bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details (
     sls_ord_num INT,
     sls_prd_key VARCHAR(50),
@@ -39,21 +42,24 @@ CREATE TABLE bronze.crm_sales_details (
     sls_price INT
 );
 
-
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
 CREATE TABLE bronze.erp_cust_az12 (
     CID VARCHAR(50),
     BDATE DATE,
     GEN VARCHAR(50)
 );
 
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
 CREATE TABLE bronze.erp_loc_a101 (
     CID VARCHAR(50),
     CNTRY VARCHAR(50)
 );
 
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
 CREATE TABLE bronze.erp_px_cat_g1v2 (
     ID VARCHAR(50),
     CAT VARCHAR(50),
     SUBCAT VARCHAR(50),
     MAINTENANCE VARCHAR(50)
 );
+
