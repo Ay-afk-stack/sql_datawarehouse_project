d489f4738e9c70d4b65ff25b3bb6ab3b9bb75c03
Ay-afk-stack <ay-afk@Ayush.localdomain>
2026-01-04 06:43:43 +0000

    Create init_schema.sql

---
+++ scripts/init_schema.session.sql
@@ -0,0 +1,16 @@
+/* 
+===========================\
+    Creating Schemas
+===========================
+Script Purpose:
+    This script creates a new schema
+*/
+
+-- Dropping Schema
+DROP SCHEMA IF EXISTS bronze;
+
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA GOLD;
+
+
