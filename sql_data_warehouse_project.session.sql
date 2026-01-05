705f7cb26300199f1b8f734d2f6c8066546fe55a
Ay-afk-stack <pakhrin.ayush404@gmail.com>
2026-01-05 11:48:35 +0000

    added stored procedure & error handling in bulk

--- sql_data_warehouse_project.session.sql
+++ sql_data_warehouse_project.session.sql
@@ -7,88 +7,75 @@ Script Purpose:
     This script also do full load in the table to update new datas in it.
 */
 
-TRUNCATE TABLE bronze.crm_cust_info;
+CREATE OR REPLACE PROCEDURE bronze.load_bronze()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    TRUNCATE TABLE bronze.crm_cust_info;
 
-COPY bronze.crm_cust_info
-FROM '/datasets/source_crm/cust_info.csv'
-WITH (
-    FORMAT CSV,
-    HEADER true,
-    DELIMITER ','
-);
+    COPY bronze.crm_cust_info
+    FROM '/datasets/source_crm/cust_info.csv'
+    WITH (
+        FORMAT CSV,
+        HEADER true,
+        DELIMITER ','
+    );
 
--- Checking Data
+    -- crm_prd_info
+    TRUNCATE TABLE bronze.crm_prd_info;
 
-SELECT COUNT(*)
-FROM bronze.crm_cust_info;
+    COPY bronze.crm_prd_info
+    FROM '/datasets/source_crm/prd_info.csv'
+    WITH (
+        FORMAT CSV,
+        HEADER true,
+        DELIMITER ','
+    );
 
--- crm_prd_info
-TRUNCATE TABLE bronze.crm_prd_info;
+    -- crm_sales_details
+    TRUNCATE TABLE bronze.crm_sales_details;
 
-COPY bronze.crm_prd_info
-FROM '/datasets/source_crm/prd_info.csv'
-WITH (
-    FORMAT CSV,
-    HEADER true,
-    DELIMITER ','
-);
+    COPY bronze.crm_sales_details
+    FROM '/datasets/source_crm/sales_details.csv'
+    WITH (
+        FORMAT CSV,
+        HEADER true,
+        DELIMITER ','
+    );
 
-SELECT COUNT(*)
-FROM bronze.crm_prd_info;
+    -- erp_cust_az12
+    TRUNCATE TABLE bronze.erp_cust_az12;
 
+    COPY bronze.erp_cust_az12
+    FROM '/datasets/source_erp/CUST_AZ12.csv'
+    WITH (
+        FORMAT CSV,
+        HEADER true,
+        DELIMITER ','
+    );
 
--- crm_sales_details
-TRUNCATE TABLE bronze.crm_sales_details;
+    -- erp_loc_a101
+    TRUNCATE TABLE bronze.erp_loc_a101;
 
-COPY bronze.crm_sales_details
-FROM '/datasets/source_crm/sales_details.csv'
-WITH (
-    FORMAT CSV,
-    HEADER true,
-    DELIMITER ','
-);
+    COPY bronze.erp_loc_a101
+    FROM '/datasets/source_erp/LOC_A101.csv'
+    WITH (
+        FORMAT CSV,
+        HEADER true,
+        DELIMITER ','
+    );
 
-SELECT COUNT(*)
-FROM bronze.crm_sales_details;
+    -- erp_px_cat_g1v2
+    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 
--- erp_cust_az12
-TRUNCATE TABLE bronze.erp_cust_az12;
+    COPY bronze.erp_px_cat_g1v2
+    FROM '/datasets/source_erp/PX_CAT_G1V2.csv'
+    WITH (
+        FORMAT CSV,
+        HEADER true,
+        DELIMITER ','
+    );  
+END;
+$$;
 
-COPY bronze.erp_cust_az12
-FROM '/datasets/source_erp/CUST_AZ12.csv'
-WITH (
-    FORMAT CSV,
-    HEADER true,
-    DELIMITER ','
-);
-
-SELECT COUNT(*)
-FROM bronze.erp_cust_az12;
-
--- erp_loc_a101
-TRUNCATE TABLE bronze.erp_loc_a101;
-
-COPY bronze.erp_loc_a101
-FROM '/datasets/source_erp/LOC_A101.csv'
-WITH (
-    FORMAT CSV,
-    HEADER true,
-    DELIMITER ','
-);
-
-SELECT COUNT(*)
-FROM bronze.erp_loc_a101;
-
--- erp_px_cat_g1v2
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-
-COPY bronze.erp_px_cat_g1v2
-FROM '/datasets/source_erp/PX_CAT_G1V2.csv'
-WITH (
-    FORMAT CSV,
-    HEADER true,
-    DELIMITER ','
-);
-
-SELECT COUNT(*)
-FROM bronze.erp_px_cat_g1v2;
+-- CALL bronze.load_bronze();
