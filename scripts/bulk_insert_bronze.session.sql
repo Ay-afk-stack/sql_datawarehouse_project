5f66fa22d2bb153bbc2ac9600d70dfdab276c662
Ay-afk-stack <pakhrin.ayush404@gmail.com>
2026-01-05 12:05:21 +0000

    calculated total duration in bulk insert of bronze

--- scripts/bulk_insert_bronze.session.sql
+++ scripts/bulk_insert_bronze.session.sql
@@ -5,90 +5,122 @@
 Script Purpose:
     This script bulk inserts the value from csv files.
     This script also do full load in the table to update new datas in it.
+    This script also have a Stored Procedure for quick execution and calculates the total duration of the process.
 */
 
-TRUNCATE TABLE bronze.crm_cust_info;
+CREATE OR REPLACE PROCEDURE bronze.load_bronze()
+LANGUAGE plpgsql
+AS $$
+DECLARE 
+    start_time TIMESTAMP;
+    end_time TIMESTAMP;
+BEGIN
 
-COPY bronze.crm_cust_info
-FROM '/datasets/source_crm/cust_info.csv'
-WITH (
-    FORMAT CSV,
-    HEADER true,
-    DELIMITER ','
-);
+    -- Time Start for log
+    start_time := clock_timestamp();
+    RAISE NOTICE '>>> Loading Bronze Layer...';
 
--- Checking Data
+    BEGIN
 
-SELECT COUNT(*)
-FROM bronze.crm_cust_info;
+        -- ERP tables with Error Handling  
+        RAISE NOTICE '>>> Loading CRM Tables';    
+        RAISE NOTICE '>>> Truncating Table: crm_cust_info';    
+        TRUNCATE TABLE bronze.crm_cust_info;
 
--- crm_prd_info
-TRUNCATE TABLE bronze.crm_prd_info;
+        RAISE NOTICE '>>> Inserting into Table: crm_cust_info';    
+        COPY bronze.crm_cust_info
+        FROM '/datasets/source_crm/cust_info.csv'
+        WITH (
+            FORMAT CSV,
+            HEADER true,
+            DELIMITER ','
+        );
 
-COPY bronze.crm_prd_info
-FROM '/datasets/source_crm/prd_info.csv'
-WITH (
-    FORMAT CSV,
-    HEADER true,
-    DELIMITER ','
-);
+        -- crm_prd_info
+        RAISE NOTICE '>>> Truncating Table: crm_prd_info';    
+        TRUNCATE TABLE bronze.crm_prd_info;
 
-SELECT COUNT(*)
-FROM bronze.crm_prd_info;
+        RAISE NOTICE '>>> Inserting into Table: crm_prd_info';    
+        COPY bronze.crm_prd_info
+        FROM '/datasets/source_crm/prd_info.csv'
+        WITH (
+            FORMAT CSV,
+            HEADER true,
+            DELIMITER ','
+        );
 
+        -- crm_sales_details
+        RAISE NOTICE '>>> Truncating Table: crm_sales_details';    
+        TRUNCATE TABLE bronze.crm_sales_details;
 
--- crm_sales_details
-TRUNCATE TABLE bronze.crm_sales_details;
+        RAISE NOTICE '>>> Inserting into Table: crm_sales_details';    
+        COPY bronze.crm_sales_details
+        FROM '/datasets/source_crm/sales_details.csv'
+        WITH (
+            FORMAT CSV,
+            HEADER true,
+            DELIMITER ','
+        );
 
-COPY bronze.crm_sales_details
-FROM '/datasets/source_crm/sales_details.csv'
-WITH (
-    FORMAT CSV,
-    HEADER true,
-    DELIMITER ','
-);
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE EXCEPTION '>>> Failed Loading ERP Table...';
+    
+    END;
 
-SELECT COUNT(*)
-FROM bronze.crm_sales_details;
+    -- ERP tables with Error Handling
 
--- erp_cust_az12
-TRUNCATE TABLE bronze.erp_cust_az12;
+    RAISE NOTICE '>>>Loading ERP Tables';
+    BEGIN
+        -- erp_cust_az12
+        RAISE NOTICE '>>> Truncating Table: erp_cust_az12';    
+        TRUNCATE TABLE bronze.erp_cust_az12;
 
-COPY bronze.erp_cust_az12
-FROM '/datasets/source_erp/CUST_AZ12.csv'
-WITH (
-    FORMAT CSV,
-    HEADER true,
-    DELIMITER ','
-);
+        RAISE NOTICE '>>> Inserting into Table: erp_cust_az12';    
+        COPY bronze.erp_cust_az12
+        FROM '/datasets/source_erp/CUST_AZ12.csv'
+        WITH (
+            FORMAT CSV,
+            HEADER true,
+            DELIMITER ','
+        );
 
-SELECT COUNT(*)
-FROM bronze.erp_cust_az12;
+        -- erp_loc_a101
+        RAISE NOTICE '>>> Truncating Table: erp_loc_a101'; 
 
--- erp_loc_a101
-TRUNCATE TABLE bronze.erp_loc_a101;
+        TRUNCATE TABLE bronze.erp_loc_a101;
+        RAISE NOTICE '>>> Inserting into Table: erp_loc_a101';    
 
-COPY bronze.erp_loc_a101
-FROM '/datasets/source_erp/LOC_A101.csv'
-WITH (
-    FORMAT CSV,
-    HEADER true,
-    DELIMITER ','
-);
+        COPY bronze.erp_loc_a101
+        FROM '/datasets/source_erp/LOC_A101.csv'
+        WITH (
+            FORMAT CSV,
+            HEADER true,
+            DELIMITER ','
+        );
 
-SELECT COUNT(*)
-FROM bronze.erp_loc_a101;
+        -- erp_px_cat_g1v2
+        RAISE NOTICE '>>> Truncating Table: erp_px_cat_g1v2';
+        TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 
--- erp_px_cat_g1v2
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+        RAISE NOTICE '>>> Inserting into Table: erp_px_cat_g1v2';
+        COPY bronze.erp_px_cat_g1v2
+        FROM '/datasets/source_erp/PX_CAT_G1V2.csv'
+        WITH (
+            FORMAT CSV,
+            HEADER true,
+            DELIMITER ','
+        );  
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE EXCEPTION '>>> Failed Loading ERP table...';
+    END;
+    
+    -- Time End for etl_log
+    end_time := clock_timestamp();
+    RAISE NOTICE '>>> Total Load Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
 
-COPY bronze.erp_px_cat_g1v2
-FROM '/datasets/source_erp/PX_CAT_G1V2.csv'
-WITH (
-    FORMAT CSV,
-    HEADER true,
-    DELIMITER ','
-);
+END;
+$$;
 
-SELECT COUNT(*)
-FROM bronze.erp_px_cat_g1v2;
+CALL bronze.load_bronze();
--- sql_data_warehouse_project.session.sql
+++ sql_data_warehouse_project.session.sql
@@ -5,77 +5,107 @@
 Script Purpose:
     This script bulk inserts the value from csv files.
     This script also do full load in the table to update new datas in it.
+    This script also have a Stored Procedure for quick execution.
 */
 
 CREATE OR REPLACE PROCEDURE bronze.load_bronze()
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    TRUNCATE TABLE bronze.crm_cust_info;
+        RAISE NOTICE 'Loading Bronze Layer...';
+    BEGIN
+        RAISE NOTICE 'Loading CRM Tables';    
+        RAISE NOTICE '>>> Truncating Table: crm_cust_info';    
+        TRUNCATE TABLE bronze.crm_cust_info;
 
-    COPY bronze.crm_cust_info
-    FROM '/datasets/source_crm/cust_info.csv'
-    WITH (
-        FORMAT CSV,
-        HEADER true,
-        DELIMITER ','
-    );
+        RAISE NOTICE '>>> Inserting into Table: crm_cust_info';    
+        COPY bronze.crm_cust_info
+        FROM '/datasets/source_crm/cust_info.csv'
+        WITH (
+            FORMAT CSV,
+            HEADER true,
+            DELIMITER ','
+        );
 
-    -- crm_prd_info
-    TRUNCATE TABLE bronze.crm_prd_info;
+        -- crm_prd_info
+        RAISE NOTICE '>>> Truncating Table: crm_prd_info';    
+        TRUNCATE TABLE bronze.crm_prd_info;
 
-    COPY bronze.crm_prd_info
-    FROM '/datasets/source_crm/prd_info.csv'
-    WITH (
-        FORMAT CSV,
-        HEADER true,
-        DELIMITER ','
-    );
+        RAISE NOTICE '>>> Inserting into Table: crm_prd_info';    
+        COPY bronze.crm_prd_info
+        FROM '/datasets/source_crm/prd_info.csv'
+        WITH (
+            FORMAT CSV,
+            HEADER true,
+            DELIMITER ','
+        );
 
-    -- crm_sales_details
-    TRUNCATE TABLE bronze.crm_sales_details;
+        -- crm_sales_details
+        RAISE NOTICE '>>> Truncating Table: crm_sales_details';    
+        TRUNCATE TABLE bronze.crm_sales_details;
 
-    COPY bronze.crm_sales_details
-    FROM '/datasets/source_crm/sales_details.csv'
-    WITH (
-        FORMAT CSV,
-        HEADER true,
-        DELIMITER ','
-    );
+        RAISE NOTICE '>>> Inserting into Table: crm_sales_details';    
+        COPY bronze.crm_sales_details
+        FROM '/datasets/source_crm/sales_details.csv'
+        WITH (
+            FORMAT CSV,
+            HEADER true,
+            DELIMITER ','
+        );
 
-    -- erp_cust_az12
-    TRUNCATE TABLE bronze.erp_cust_az12;
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE EXCEPTION 'Failed Loading ERP Table...';
+    
+    END;
 
-    COPY bronze.erp_cust_az12
-    FROM '/datasets/source_erp/CUST_AZ12.csv'
-    WITH (
-        FORMAT CSV,
-        HEADER true,
-        DELIMITER ','
-    );
+        RAISE NOTICE 'Loading ERP Tables';
 
-    -- erp_loc_a101
-    TRUNCATE TABLE bronze.erp_loc_a101;
+    BEGIN
+        -- erp_cust_az12
+        RAISE NOTICE '>>> Truncating Table: erp_cust_az12';    
+        TRUNCATE TABLE bronze.erp_cust_az12;
 
-    COPY bronze.erp_loc_a101
-    FROM '/datasets/source_erp/LOC_A101.csv'
-    WITH (
-        FORMAT CSV,
-        HEADER true,
-        DELIMITER ','
-    );
+        RAISE NOTICE '>>> Inserting into Table: erp_cust_az12';    
+        COPY bronze.erp_cust_az12
+        FROM '/datasets/source_erp/CUST_AZ12.csv'
+        WITH (
+            FORMAT CSV,
+            HEADER true,
+            DELIMITER ','
+        );
 
-    -- erp_px_cat_g1v2
-    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+        -- erp_loc_a101
+        RAISE NOTICE '>>> Truncating Table: erp_loc_a101'; 
 
-    COPY bronze.erp_px_cat_g1v2
-    FROM '/datasets/source_erp/PX_CAT_G1V2.csv'
-    WITH (
-        FORMAT CSV,
-        HEADER true,
-        DELIMITER ','
-    );  
+        TRUNCATE TABLE bronze.erp_loc_a101;
+        RAISE NOTICE '>>> Inserting into Table: erp_loc_a101';    
+
+        COPY bronze.erp_loc_a101
+        FROM '/datasets/source_erp/LOC_A101.csv'
+        WITH (
+            FORMAT CSV,
+            HEADER true,
+            DELIMITER ','
+        );
+
+        -- erp_px_cat_g1v2
+        RAISE NOTICE '>>>Truncating Table: erp_px_cat_g1v2';
+        TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+        RAISE NOTICE '>>>Inserting into Table: erp_px_cat_g1v2';
+        COPY bronze.erp_px_cat_g1v2
+        FROM '/datasets/source_erp/PX_CAT_G1V2.csv'
+        WITH (
+            FORMAT CSV,
+            HEADER true,
+            DELIMITER ','
+        );  
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE EXCEPTION 'Failed Loading ERP table...';
+    END;
 END;
 $$;
 
--- CALL bronze.load_bronze();
+CALL bronze.load_bronze();
