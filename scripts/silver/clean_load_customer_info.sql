cd91b427bc63c359070c914a2551afaeb0b9638f
Ay-afk-stack <pakhrin.ayush404@gmail.com>
2026-01-06 13:09:10 +0000

    added clean & load customer_info silver layer

---
+++ scripts/silver/clean_load_customer_info.sql
@@ -0,0 +1,34 @@
+-- Inserting Standardized Data into Silver tables
+TRUNCATE TABLE silver.crm_cust_info;
+
+INSERT INTO silver.crm_cust_info (
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_marital_status,
+	cst_gndr,
+	cst_create_date
+)
+SELECT 
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+CASE WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+	 WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+	 ELSE 'Unknown'
+END AS cst_marital_status, -- Normalize marital status values to readable format
+CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+	 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+	 ELSE 'Unknown'
+END AS cst_gndr, -- Normalize Gender Values to Readable Format
+cst_create_date 
+FROM (SELECT 
+		*,
+		ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+		FROM bronze.crm_cust_info cci 
+		WHERE cst_id IS NOT NULL
+) t
+WHERE flag_last = 1; -- SELECT the most recent record per customer
+
