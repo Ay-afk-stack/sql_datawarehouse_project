cc9ede1efd2b9fd3a9c29cdf581c4960803735b3
Ay-afk-stack <pakhrin.ayush404@gmail.com>
2026-01-06 12:29:13 +0000

    init tables for silver layer

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -15,7 +15,8 @@ CREATE TABLE silver.crm_cust_info (
     cst_lastname VARCHAR(50),
     cst_marital_status VARCHAR(50),
     cst_gndr VARCHAR(50),
-    cst_create_date DATE
+    cst_create_date DATE,
+    dwh_create_date TIMESTAMP DEFAULT NOW()
 );
 
 DROP TABLE IF EXISTS silver.crm_prd_info;
@@ -26,7 +27,8 @@ CREATE TABLE silver.crm_prd_info (
     prd_cost INT,
     prd_line VARCHAR(50),
     prd_start_dt DATE,
-    prd_end_dt DATE
+    prd_end_dt DATE,
+    dwh_create_date TIMESTAMP DEFAULT NOW()
 );
 
 DROP TABLE IF EXISTS silver.crm_sales_details;
@@ -39,20 +41,23 @@ CREATE TABLE silver.crm_sales_details (
     sls_due_dt INT,
     sls_sales INT,
     sls_quantity INT,
-    sls_price INT
+    sls_price INT,
+    dwh_create_date TIMESTAMP DEFAULT NOW()
 );
 
 DROP TABLE IF EXISTS silver.erp_cust_az12;
 CREATE TABLE silver.erp_cust_az12 (
     CID VARCHAR(50),
     BDATE DATE,
-    GEN VARCHAR(50)
+    GEN VARCHAR(50),
+    dwh_create_date TIMESTAMP DEFAULT NOW()
 );
 
 DROP TABLE IF EXISTS silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101 (
     CID VARCHAR(50),
-    CNTRY VARCHAR(50)
+    CNTRY VARCHAR(50),
+    dwh_create_date TIMESTAMP DEFAULT NOW()
 );
 
 DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
@@ -60,6 +65,7 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     ID VARCHAR(50),
     CAT VARCHAR(50),
     SUBCAT VARCHAR(50),
-    MAINTENANCE VARCHAR(50)
+    MAINTENANCE VARCHAR(50),
+    dwh_create_date TIMESTAMP DEFAULT NOW()
 );
 
