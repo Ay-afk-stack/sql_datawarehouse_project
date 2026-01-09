ff2e39be2a2bc1ad575f65432b261692755d770d
Ay-afk-stack <pakhrin.ayush404@gmail.com>
2026-01-09 14:54:40 +0000

    tested silver_erp_cust_az12 data

---
+++ tests/test_silver_erp_cust_az12.sql
@@ -0,0 +1,18 @@
+-- Identify Out-of-Range Dates
+
+SELECT DISTINCT 
+bdate
+FROM silver.erp_cust_az12
+WHERE bdate < '1924-01-01' OR bdate > CURRENT_DATE;
+
+-- Data Standarization & Consistency
+SELECT DISTINCT
+  gen,
+  CASE WHEN UPPER(TRIM(gen)) IN ('F','FEMALE') THEN 'Female'
+       WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+       ELSE 'n/a'
+  END AS gen
+FROM silver.erp_cust_az12;
+
+SELECT *
+FROM silver.erp_cust_az12;
